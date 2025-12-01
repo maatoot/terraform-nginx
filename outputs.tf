a98aac3b8916db14b6078cb1d58f357ab16609e4
Hossam Hassan <81199955+maatoot@users.noreply.github.com>
2025-12-01 22:02:01 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_public_ip" {
-  value = aws_instance.nginx_server.public_ip
+  value = aws_instance.main_server.public_ip
 }
 
 output "nginx_url" {
-  value = "http://${aws_instance.nginx_server.public_ip}"
+  value = "http://${aws_instance.main_server.public_ip}"
 }
